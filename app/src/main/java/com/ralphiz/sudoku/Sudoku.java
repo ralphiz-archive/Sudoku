6b24df8e9d927c7526419c1db9d5bc74b82a1494
Ralph Cacho <cacho.rc@gmail.com>
2014-11-09 22:52:09 -0800

    Add landscape layout

--- app/src/main/java/com/ralphiz/sudoku/Sudoku.java
+++ app/src/main/java/com/ralphiz/sudoku/Sudoku.java
@@ -1,7 +1,7 @@
 package com.ralphiz.sudoku;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,6 +12,10 @@ public class Sudoku extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sudoku);
+
+        // Hide action bar
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
     }
 
 
